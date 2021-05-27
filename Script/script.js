9a801c3109baf5dbf912dab5110087195f7895cd
moayadalhaj <>
2021-05-27 12:14:56 +0000

    Insert Javascrip to my webpage

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,12 @@
 var userName = prompt("Please Enter Your Name :");
 var age = prompt("Please Enter Your Age :");
 
-alert("Welcome" + userName);
-domcumnt.write("<p>Welcome<p>" + userName)
-var btn = confirm("Press a button!");
+alert("Welcome " + userName);
+document.write("<span>Welcome</span> " + userName + " <span>Hava a nice day!</span>") 
+var btn = confirm("Do You Have A Car?");
   if (btn == true) {
-    document.write("You Can Order what do you want ");
+    txt = ("You Can Order what do you want");
   } else {
-    doucument.write = "Choose your favorite car";
+    txt = ("Choose your favorite car");
   }
+  document.write ("<br>" + txt)
