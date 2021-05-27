0506f5242ad6b74a9c240ce4dfeb64dd35c0170f
moayadalhaj <>
2021-05-27 11:41:33 +0000

    Insert javascript im my webpage

---
+++ Script/script.js
@@ -0,0 +1,6 @@
+var userName = prompt("Please Enter Your Name :");
+var age = prompt("Please Enter Your Age :");
+
+alert("Welcome" + userName);
+domcumnt.write("<p>Welcome<p>" + userName)
+confirm("Do You Have A Car: ")
