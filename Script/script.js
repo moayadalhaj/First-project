1e226dec6489c66eb547ba538ef6a3fa2fe84e08
moayadalhaj <>
2021-05-27 11:46:41 +0000

    Insert javascript

--- Script/script.js
+++ Script/script.js
@@ -3,4 +3,9 @@ var age = prompt("Please Enter Your Age :");
 
 alert("Welcome" + userName);
 domcumnt.write("<p>Welcome<p>" + userName)
-confirm("Do You Have A Car: ")
+var btn = confirm("Press a button!");
+  if (btn == true) {
+    document.write("You Can Order what do you want ");
+  } else {
+    doucument.write = "Choose your favorite car";
+  }
