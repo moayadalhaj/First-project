89f7a10e5ef97bced157179da6c1af8c14f1ed10
Moayad Alhaj <84705318+moayadalhaj@users.noreply.github.com>
2021-05-27 15:26:36 +0300

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -2,7 +2,9 @@ var userName = prompt("Please Enter Your Name :");
 var age = prompt("Please Enter Your Age :");
 
 alert("Welcome " + userName);
+
 document.write("<span>Welcome</span> " + userName + " <span>Hava a nice day!</span>") 
+
 var btn = confirm("Do You Have A Car?");
   if (btn == true) {
     txt = ("You Can Order what do you want");
